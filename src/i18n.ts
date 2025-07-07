283c06faa25b70c4518152e39101af92bfd4775d
lianet <jack.tolmak@yandex.ru>
2025-07-07 16:39:23 +0300

    add translations languages

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,16 +2,16 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import enTranslation from "./locales/en/translation.json";
+import kkTranslation from "./locales/kk/translation.json";
 import ruTranslation from "./locales/ru/translation.json";
 
 export type Resources = {
-	translation: typeof enTranslation;
+	translation: typeof kkTranslation;
 };
 
 const resources = {
-	en: {
-		translation: enTranslation,
+	kk: {
+		translation: kkTranslation,
 	},
 	ru: {
 		translation: ruTranslation,
@@ -20,7 +20,7 @@ const resources = {
 
 declare module "i18next" {
 	interface CustomTypeOptions {
-		resources: (typeof resources)["en"]; // Здесь указываем тип для ресурсов,
+		resources: (typeof resources)["ru"]; // Здесь указываем тип для ресурсов,
 		// чтобы t() знал о ваших ключах.
 		// Можно также использовать { en: Resources; ru: Resources; }
 	}
